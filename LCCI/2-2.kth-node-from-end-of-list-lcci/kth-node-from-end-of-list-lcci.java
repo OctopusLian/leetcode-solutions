a1b91cc25e6ca7c3dbbc3032b3405d92b8ad6a90
OctopusLian <zoctopus@qq.com>
2020-02-17 22:16:05 +0800

    update 2-16

---
+++ LCCI/2-2.kth-node-from-end-of-list-lcci/kth-node-from-end-of-list-lcci.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthToLast(ListNode head, int k) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        for (int i = 0;i < k;i++) {
+            if (p1 == null) return -1;
+            p1 = p1.next;
+        }
+        while(p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2.val;
+    }
+}
