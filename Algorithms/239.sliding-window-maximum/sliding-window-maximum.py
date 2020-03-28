bbb2e0bc8ac4b0c57b507c4d168118243c8b8c04
OctopusLian <zoctopus@qq.com>
2020-03-28 13:05:29 +0800

    add two stack solutions

---
+++ Algorithms/239.sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        if nums != []:
+            tmp_max = max(nums[0:k])
+            for i in range(len(nums) - (k-1)):
+                tmp_max = max(nums[i:i+k])
+                res.append(tmp_max)
+            return res
