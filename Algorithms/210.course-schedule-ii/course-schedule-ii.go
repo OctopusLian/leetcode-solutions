1e9d716999124b711d607e6df3c9bb66e8deed89
OctopusLian <zoctopus@qq.com>
2020-05-17 11:27:02 +0800

    punch card,add 210.course-schedule-ii go solution

---
+++ Algorithms/210.course-schedule-ii/course-schedule-ii.go
@@ -0,0 +1,36 @@
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	//解法1，广度优先搜索
+    var (
+        edges = make([][]int, numCourses)
+        indeg = make([]int, numCourses)
+        result []int
+    )
+
+    for _, info := range prerequisites {
+        edges[info[1]] = append(edges[info[1]], info[0])
+        indeg[info[0]]++
+    }
+
+    q := []int{}
+    for i := 0; i < numCourses; i++ {
+        if indeg[i] == 0 {
+            q = append(q, i)
+        }
+    }
+
+    for len(q) > 0 {
+        u := q[0]
+        q = q[1:]
+        result = append(result, u)
+        for _, v := range edges[u] {
+            indeg[v]--
+            if indeg[v] == 0 {
+                q = append(q, v)
+            }
+        }
+    }
+    if len(result) != numCourses {
+        return []int{}
+    }
+    return result
+}
