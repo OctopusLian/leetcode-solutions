ed6b49122e8bca7e5f7cdf59aa9e039178a68ce9
OctopusLian <zoctopus@qq.com>
2020-07-25 10:16:15 +0800

    punch card,add algo 410.split-array-largest-sum dp and greedy+er-fen search solutions

---
+++ Algorithms/410.split-array-largest-sum/split-array-largest-sum-2.go
@@ -0,0 +1,38 @@
+func splitArray(nums []int, m int) int {
+	//解法2，动态规划
+    n := len(nums)
+    f := make([][]int, n + 1)
+    sub := make([]int, n + 1)
+    for i := 0; i < len(f); i++ {
+        f[i] = make([]int, m + 1)
+        for j := 0; j < len(f[i]); j++ {
+            f[i][j] = math.MaxInt32
+        }
+    }
+    for i := 0; i < n; i++ {
+        sub[i + 1] = sub[i] + nums[i]
+    }
+    f[0][0] = 0
+    for i := 1; i <= n; i++ {
+        for j := 1; j <= min(i, m); j++ {
+            for k := 0; k < i; k++ {
+                f[i][j] = min(f[i][j], max(f[k][j - 1], sub[i] - sub[k]))
+            }
+        }
+    }
+    return f[n][m]
+}
+
+func min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
