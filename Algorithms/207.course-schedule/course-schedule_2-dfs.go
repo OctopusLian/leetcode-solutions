1b037e876a09f7a4d05d63c42d5421bf14ef9146
OctopusLian <zoctopus@qq.com>
2020-08-04 07:50:46 +0800

    punch card,add algo 207.course-schedule bfs-dfs solutions

---
+++ Algorithms/207.course-schedule/course-schedule_2-dfs.go
@@ -0,0 +1,38 @@
+func canFinish(numCourses int, prerequisites [][]int) bool {
+    //解法2，深度优先搜索
+    var (
+        edges = make([][]int, numCourses)
+        visited = make([]int, numCourses)
+        result []int
+        valid = true
+        dfs func(u int)
+    )
+
+    dfs = func(u int) {
+        visited[u] = 1
+        for _, v := range edges[u] {
+            if visited[v] == 0 {
+                dfs(v)
+                if !valid {
+                    return
+                }
+            } else if visited[v] == 1 {
+                valid = false
+                return
+            }
+        }
+        visited[u] = 2
+        result = append(result, u)
+    }
+
+    for _, info := range prerequisites {
+        edges[info[1]] = append(edges[info[1]], info[0])
+    }
+
+    for i := 0; i < numCourses && valid; i++ {
+        if visited[i] == 0 {
+            dfs(i)
+        }
+    }
+    return valid
+}
