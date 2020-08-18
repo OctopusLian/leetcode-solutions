c29585df417c850538ea319eec3776fabc2b7901
OctopusLian <zoctopus@qq.com>
2020-08-19 07:31:08 +0800

    punch card,add algo 647.palindromic-substrings go solution

---
+++ Algorithms/647.palindromic-substrings/palindromic-substrings.go
@@ -0,0 +1,14 @@
+func countSubstrings(s string) int {
+	//解法1，中心拓展
+    n := len(s)
+    ans := 0
+    for i := 0; i < 2 * n - 1; i++ {
+        l, r := i / 2, i / 2 + i % 2
+        for l >= 0 && r < n && s[l] == s[r] {
+            l--
+            r++
+            ans++
+        }
+    }
+    return ans
+}
