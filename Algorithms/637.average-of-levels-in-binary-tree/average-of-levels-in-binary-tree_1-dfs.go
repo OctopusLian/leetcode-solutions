e7ab03a0e49f2698f4479051c8d99af9a0cfeab2
OctopusLian <zoctopus@qq.com>
2020-09-12 11:01:32 +0800

    punch card,add algo 637.average-of-levels-in-binary-tree dfs go solution

---
+++ Algorithms/637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree_1-dfs.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type data struct{ sum, count int }
+
+func averageOfLevels(root *TreeNode) []float64 {
+	//解法1，深度优先搜索
+	 levelData := []data{}
+	 var dfs func(node *TreeNode, level int)
+	 dfs = func(node *TreeNode, level int) {
+		 if node == nil {
+			 return
+		 }
+		 if level < len(levelData) {
+			 levelData[level].sum += node.Val
+			 levelData[level].count++
+		 } else {
+			 levelData = append(levelData, data{node.Val, 1})
+		 }
+		 dfs(node.Left, level+1)
+		 dfs(node.Right, level+1)
+	 }
+	 dfs(root, 0)
+ 
+	 averages := make([]float64, len(levelData))
+	 for i, d := range levelData {
+		 averages[i] = float64(d.sum) / float64(d.count)
+	 }
+	 return averages
+}
