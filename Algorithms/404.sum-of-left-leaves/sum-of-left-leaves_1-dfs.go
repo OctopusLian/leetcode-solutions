cf2e65d5b4f22a307935b1f1e1ef503d2522d3c4
OctopusLian <zoctopus@qq.com>
2020-09-19 09:18:01 +0800

    punch card,add algo 404.sum-of-left-leaves dfs go solution

---
+++ Algorithms/404.sum-of-left-leaves/sum-of-left-leaves_1-dfs.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func isLeafNode(node *TreeNode) bool {
+    return node.Left == nil && node.Right == nil
+}
+
+func dfs(node *TreeNode) (ans int) {
+    if node.Left != nil {
+        if isLeafNode(node.Left) {
+            ans += node.Left.Val
+        } else {
+            ans += dfs(node.Left)
+        }
+    }
+    if node.Right != nil && !isLeafNode(node.Right) {
+        ans += dfs(node.Right)
+    }
+    return
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	//解法1，深度优先搜索
+    if root == nil {
+        return 0
+    }
+    return dfs(root)
+}
