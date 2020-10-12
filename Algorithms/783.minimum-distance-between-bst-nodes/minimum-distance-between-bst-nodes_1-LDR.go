76be0b2f8d8b9919bdb543f6fffd5a8829c08422
OctopusLian <zoctopus@qq.com>
2020-10-12 08:14:54 +0800

    punch card,add 530.minimum-absolute-difference-in-bst and 783.minimum-distance-between-bst-nodes LDR go solution

---
+++ Algorithms/783.minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes_1-LDR.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+	//解法1，中序遍历
+    ans, pre := math.MaxInt64, -1
+    var dfs func(*TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        dfs(node.Left)
+        if pre != -1 && node.Val-pre < ans {
+            ans = node.Val - pre
+        }
+        pre = node.Val
+        dfs(node.Right)
+    }
+    dfs(root)
+    return ans
+}
