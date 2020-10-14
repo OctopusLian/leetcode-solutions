dd46758bef5dd8536555833c3b3c3a20171c78ea
OctopusLian <zoctopus@qq.com>
2020-10-15 07:50:14 +0800

    punch card,add algo 116.populating-next-right-pointers-in-each-node level-traversal go solution

---
+++ Algorithms/116.populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node_1-level-traversal.go
@@ -0,0 +1,44 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	//解法1,层次遍历
+	if root == nil {
+        return root
+    }
+
+    // 初始化队列同时将第一层节点加入队列中，即根节点
+    queue := []*Node{root}
+
+    // 循环迭代的是层数
+    for len(queue) > 0 {
+        tmp := queue
+        queue = nil
+
+        // 遍历这一层的所有节点
+        for i, node := range tmp {
+            // 连接
+            if i+1 < len(tmp) {
+                node.Next = tmp[i+1]
+            }
+
+            // 拓展下一层节点
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+    }
+
+    // 返回根节点
+    return root
+}
