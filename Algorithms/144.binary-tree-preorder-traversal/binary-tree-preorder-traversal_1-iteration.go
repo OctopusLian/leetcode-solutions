c6d3e1ee42591f6cac07af1d8e331842d4e35a6f
OctopusLian <zoctopus@qq.com>
2020-10-27 08:32:30 +0800

    punch card,add algo binary-tree-preorder-traversal_1-iteration go solution

---
+++ Algorithms/144.binary-tree-preorder-traversal/binary-tree-preorder-traversal_1-iteration.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode)(vals []int) {
+	//解法1，迭代
+    stack := []*TreeNode{}
+    node := root
+    for node != nil || len(stack) > 0 {
+        for node != nil {
+            vals = append(vals, node.Val)
+            stack = append(stack, node)
+            node = node.Left
+        }
+        node = stack[len(stack)-1].Right
+        stack = stack[:len(stack)-1]
+    }
+    return
+}
