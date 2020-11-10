c2037189826c70bf8f26d723210f6f95a8979201
OctopusLian <zoctopus@qq.com>
2020-11-10 08:41:21 +0800

    punch card,add algo 31.next-permutation recursion cpp solution and two-scan go solution

---
+++ Algorithms/31.next-permutation/next-permutation_1-recursion.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        //解法1，递归
+        next_permutation(nums.begin(), nums.end());
+    }
+};
---
+++ Algorithms/31.next-permutation/next-permutation_1-two-scan.go
@@ -0,0 +1,22 @@
+func nextPermutation(nums []int) {
+	//解法1，两边扫描
+    n := len(nums)
+    i := n - 2
+    for i >= 0 && nums[i] >= nums[i+1] {
+        i--
+    }
+    if i >= 0 {
+        j := n - 1
+        for j >= 0 && nums[i] >= nums[j] {
+            j--
+        }
+        nums[i], nums[j] = nums[j], nums[i]
+    }
+    reverse(nums[i+1:])
+}
+
+func reverse(a []int) {
+    for i, n := 0, len(a); i < n/2; i++ {
+        a[i], a[n-1-i] = a[n-1-i], a[i]
+    }
+}as
