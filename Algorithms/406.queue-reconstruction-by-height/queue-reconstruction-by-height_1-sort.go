26192452d52b2bef881bcbdcf3f6a7afc05e0ff6
OctopusLian <zoctopus@qq.com>
2020-11-16 21:53:34 +0800

    punch card,add algo 406.queue-reconstruction-by-height go solution

---
+++ Algorithms/406.queue-reconstruction-by-height/queue-reconstruction-by-height_1-sort.go
@@ -0,0 +1,21 @@
+func reconstructQueue(people [][]int) [][]int {
+	//解法1，排序
+    sort.Slice(people, func(i, j int) bool {
+        a, b := people[i], people[j]
+        return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+    })
+    ans := make([][]int, len(people))
+    for _, person := range people {
+        spaces := person[1] + 1
+        for i := range ans {
+            if ans[i] == nil {
+                spaces--
+                if spaces == 0 {
+                    ans[i] = person
+                    break
+                }
+            }
+        }
+    }
+    return ans
+}
